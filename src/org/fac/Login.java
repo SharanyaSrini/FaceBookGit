e849f1df3e9d1d93fe0e6f3ec06e222c9b997244
SharanyaSrini <srinisaran1993@gmail.com>
2021-12-08 15:38:22 -0800

    second updated code

--- src/org/fac/Login.java
+++ src/org/fac/Login.java
@@ -1,5 +1,24 @@
 package org.fac;
 
 public class Login {
+	
+	public static void b() {
+		
+		
+		
+		System.out.println("B");
+
+		
+		
+	}
+public static void main(String[] args) {
+	
+	
+	b();
+	
+	
+	
+}
+
 
 }
